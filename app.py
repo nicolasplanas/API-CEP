92f66a6f26487ae2b7cb0a20e2a9e251eb00ed6b
nicolasplanas <nicolasplanasgomes27@gmail.com>
2025-05-24 11:51:19 -0300

    Atualização visual 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -8,44 +8,45 @@ def hello():
     return render_template('home.html')
 
 # endpoint - pesquisar endereço através do cep, retorna em formato json
-@app.route('/pesquisacep/<cep>', methods=['GET'])
-def pesquisacep(cep):
+@app.route('/pesquisacep', methods=['GET', 'POST'])
+def pesquisacep():
 
-    url      = f"https://viacep.com.br/ws/{cep}/json/"
-    resposta = requests.get(url)
-    return resposta.json()
+    if  request.method == 'GET':
+        return render_template("paginacep.html")
+    
+    elif request.method == 'POST':
+        cep      = request.form['cep']
+        url      = f"https://viacep.com.br/ws/{cep}/json/"
+        resposta = requests.get(url).json()
+        return render_template("paginacep.html", **resposta)
 
-@app.route('/city-search/<city>', methods=['GET'])
-def searchcity(city):
+def get_tempo(cidade):
 
     key      = "c4380707dde242f4b78202712252204"
-    url      = f"https://api.weatherapi.com/v1/current.json?key={key}&q={city}&lang=pt"
+    url      = f"https://api.weatherapi.com/v1/current.json?key={key}&q={cidade}&lang=pt"
+    result   = requests.get(url).json()
 
-    resposta = requests.get(url)
-    result   = resposta.json()
+    return {"temperatura" : result["current"]["temp_c"],
+            "umidade"     : result["current"]["humidity"],
+            "velvento"    : result["current"]["vis_km"],
+            "pressaoatim" : result["current"]["pressure_mb"],
+            "cidade"      : result["location"]["name"],
+            "regiao"      : result["location"]["region"],
+            "localhora"   : result["location"]["localtime"],
+            "pais"        : result["location"]["country"]}
 
-    # Verifica se veio erro na resposta
-    if "error" in result:
-        return jsonify({"erro": "Cidade não encontrada ou chave da API inválida."}), 404
+#retorna a previsão do tempo
+@app.route('/tempo', methods=['GET', 'POST'])
+def tempo():
 
-    temperatura = result["current"]["temp_c"]
-    umidade     = result["current"]["humidity"]
-    velvento    = result["current"]["vis_km"]
-    pressaoatim = result["current"]["pressure_mb"]
-    cidade      = result["location"]["name"]
-    regiao      = result["location"]["region"]
-    localhora   = result["location"]["localtime"]
-    pais        = result["location"]["country"]
+    if request.method == 'GET':
+        result = get_tempo("São Paulo")
+        return render_template("paginatempo.html", **result)
 
-    return render_template("paginatempo.html",
-                            temp=temperatura, 
-                            umid=umidade,
-                            vento=velvento,
-                            pressao=pressaoatim,
-                            nome=cidade, 
-                            estado=regiao, 
-                            horalocal=localhora,
-                            pais=pais)
+    elif request.method == 'POST':
+        cidade = request.form['cidade']
+        result = get_tempo(cidade)
+        return render_template("paginatempo.html", **result)
 
 if __name__ == '__main__':
     app.run(debug=True)
