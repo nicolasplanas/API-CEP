79515e2d218881512ed6bf972da1f6d5ed6fa01d
nicolasplanas <nicolasplanasgomes27@gmail.com>
2025-05-25 22:09:52 -0300

    Atualização visual 2

--- app.py
+++ app.py
@@ -3,28 +3,49 @@ import requests
 
 app = Flask(__name__)
 
+# endpoint - página principal do projeto
 @app.route('/')
 def hello():
     return render_template('home.html')
 
-# endpoint - pesquisar endereço através do cep, retorna em formato json
-@app.route('/pesquisacep', methods=['GET', 'POST'])
+# endpoint - pesquisa de endereço através do cep
+@app.route('/cep', methods=['GET', 'POST'])
 def pesquisacep():
 
-    if  request.method == 'GET':
+    if   request.method == 'GET':
         return render_template("paginacep.html")
     
     elif request.method == 'POST':
         cep      = request.form['cep']
         url      = f"https://viacep.com.br/ws/{cep}/json/"
         resposta = requests.get(url).json()
+        if "erro" in resposta:
+            return render_template("paginacep.html", erro=True)
+        
         return render_template("paginacep.html", **resposta)
 
+# endpoint - pesquisa de previsão do tempo
+@app.route('/tempo', methods=['GET', 'POST'])
+def tempo():
+
+    if   request.method == 'GET':
+        return render_template("paginatempo.html")
+ 
+    elif request.method == 'POST':
+        cidade = request.form.get('cidade')
+
+        if not cidade or cidade == "Nenhum":
+            return render_template("paginatempo.html", erro=True)
+        
+        result = get_tempo(cidade)
+        return render_template("paginatempo.html", **result)
+    
+# função - retorna a previsão do tempo para o endpoint
 def get_tempo(cidade):
 
-    key      = "c4380707dde242f4b78202712252204"
-    url      = f"https://api.weatherapi.com/v1/current.json?key={key}&q={cidade}&lang=pt"
-    result   = requests.get(url).json()
+    key    = "c4380707dde242f4b78202712252204"
+    url    = f"https://api.weatherapi.com/v1/current.json?key={key}&q={cidade}&lang=pt"
+    result = requests.get(url).json()
 
     return {"temperatura" : result["current"]["temp_c"],
             "umidade"     : result["current"]["humidity"],
@@ -35,18 +56,6 @@ def get_tempo(cidade):
             "localhora"   : result["location"]["localtime"],
             "pais"        : result["location"]["country"]}
 
-#retorna a previsão do tempo
-@app.route('/tempo', methods=['GET', 'POST'])
-def tempo():
-
-    if request.method == 'GET':
-        result = get_tempo("São Paulo")
-        return render_template("paginatempo.html", **result)
-
-    elif request.method == 'POST':
-        cidade = request.form['cidade']
-        result = get_tempo(cidade)
-        return render_template("paginatempo.html", **result)
-
+# inicia o app
 if __name__ == '__main__':
     app.run(debug=True)
